3ab0924f152585ed974eebcdff7990371c448007
VolodymyrTabarkevych <w.tabarkevych@gmail.com>
2020-04-13 11:00:25 +0300

    Update READMY file and fix minor issue

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-Task 10 - Models
-Create a JAVA project based on the University UML class diagram from the previous task.
+### Task 11 - DAO layer
+Create Spring JDBC based DAO for your application.
+
+1. make.sh    # Builds the project (clears dist/ dir and repopulates it with JAR and etc.) except the db image
+2. make-db.sh # Builds Docker image of database and pushes it to Docker registry (hub.docker.com if not specified otherwise)
+3. dist/:
+    1. university.sh
+    2. university-SNAPHOST-1.0.jar
+    3. university-db.sh # Runs docker image, must be executed before the `university.sh`
+    4. README.md # Readme file with how-to-run for the end-user / admin who will install/deploy univeristy app on the production server
+4. README.md  # Readme for a developer, about opening the project in the IDE, what tools/versions required to build it, etc.```
+
+### Make executable jar files
+
+In project directory run one of "make" sctipts, depending on your operating system(make.sh for Mac or Linux, make.bat from Windows).
+Make script will save jar file in directory `./dist`
+
+### Run project
+If you have already created "university" database with user "manager" on localhost 5432 you can simply run "university" script. 
+If not you need to run "make-db" script(to create docker image) and then run "university-db" sctipt to run docker image.
--- src/main/java/ua/com/foxminded/university/DbConnection.java
+++ src/main/java/ua/com/foxminded/university/DbConnection.java
@@ -1,6 +1,5 @@
 package ua.com.foxminded.university;
 
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Properties;
 
@@ -14,24 +13,19 @@ public class DbConnection {
 
     public DbConnection() {
         props = new Properties();
-        try (FileInputStream in = new FileInputStream("src/main/resources/database.properties")) {
-            props.load(in);
+        try {
+            props.load(DbConnection.class.getResourceAsStream("/database.properties"));
         } catch (IOException e) {
-            System.out.println("Cannot read the file: " + e.getMessage());
+            System.out.println(e.getMessage());
         }
     }
 
     public DriverManagerDataSource init() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        /*
-         * dataSource.setUsername(props.getProperty("user"));
-         * dataSource.setPassword(props.getProperty("password"));
-         * dataSource.setUrl(props.getProperty("url"));
-         * dataSource.setDriverClassName("org.postgresql.Driver");
-         */
-        dataSource.setUsername("manager");
-        dataSource.setPassword("1111");
-        dataSource.setUrl("jdbc:postgresql://localhost:5432/university");
+
+        dataSource.setUsername(props.getProperty("user"));
+        dataSource.setPassword(props.getProperty("password"));
+        dataSource.setUrl(props.getProperty("url"));
         dataSource.setDriverClassName("org.postgresql.Driver");
         return dataSource;
     }
