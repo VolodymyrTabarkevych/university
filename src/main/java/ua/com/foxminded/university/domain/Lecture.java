6b1e49421ae4bd37451678f0a20b607206df0d24
VolodymyrTabarkevych <w.tabarkevych@gmail.com>
2020-04-17 12:47:17 +0300

    Lecture now use lombok builder

--- src/main/java/ua/com/foxminded/university/domain/Lecture.java
+++ src/main/java/ua/com/foxminded/university/domain/Lecture.java
@@ -3,23 +3,22 @@ package ua.com.foxminded.university.domain;
 import java.time.LocalDate;
 import java.time.LocalTime;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
-@Getter@Setter
+@Getter
+@Setter
+@Builder
 public class Lecture {
-    private int lectureId = 0;
-    private Subject subject = new Subject("");
-    private Teacher teacher = new Teacher(0, "", "");
-    private Group group = new Group("");
-    private Room room = new Room(0);
-    private LocalDate date = LocalDate.of(1, 1, 1);
-    private LocalTime startTime = LocalTime.of(0, 0);
-    private LocalTime endTime = LocalTime.of(0, 0);;
-
-    private Lecture() {
-
-    }
+    private int lectureId;
+    private Subject subject;
+    private Teacher teacher;
+    private Group group;
+    private Room room;
+    private LocalDate date;
+    private LocalTime startTime;
+    private LocalTime endTime;
 
     @Override
     public String toString() {
@@ -28,58 +27,5 @@ public class Lecture {
                 + "Room: " + room.getNumber() + System.lineSeparator() + "Date: " + date.toString()
                 + System.lineSeparator() + "Lecture begin: " + startTime.toString() + " Lecture end: "
                 + endTime.toString();
-    }
-
-    public static class Builder {
-        Lecture lecture;
-
-        public Builder() {
-            lecture = new Lecture();
-        }
-
-        public Builder setLectureId(int lectureId) {
-            lecture.lectureId = lectureId;
-            return this;
-        }
-
-        public Builder setSubject(Subject subject) {
-            lecture.subject = subject;
-            return this;
-
-        }
-
-        public Builder setTeacher(Teacher teacher) {
-            lecture.teacher = teacher;
-            return this;
-        }
-
-        public Builder setGroup(Group group) {
-            lecture.group = group;
-            return this;
-        }
-
-        public Builder setDate(LocalDate date) {
-            lecture.date = date;
-            return this;
-        }
-
-        public Builder setStartTime(LocalTime startTime) {
-            lecture.startTime = startTime;
-            return this;
-        }
-
-        public Builder setEndTime(LocalTime endTime) {
-            lecture.endTime = endTime;
-            return this;
-        }
-
-        public Builder setRoom(Room room) {
-            lecture.room = room;
-            return this;
-        }
-
-        public Lecture build() {
-            return lecture;
-        }
     }
 }
